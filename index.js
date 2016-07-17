f15bd4f013eb3cb58bee56ea4cb40ef55b22f2a6
Andreas Lind <andreas@one.com>
2016-07-17 12:37:17 +0200

    Fix parsing of UTC timestamps, make the test suite independent of the local time zone.

--- index.js
+++ index.js
@@ -140,8 +140,8 @@ function parseDate(string) {
   var match = string.match(/^(\d{4}):(\d{2}):(\d{2}) (\d{2}):(\d{2}):(\d{2})$/);
   if (!match)
     return null;
-  
-  return new Date(
+
+  return new Date(Date.UTC(
     match[1],
     match[2] - 1,
     match[3],
@@ -149,7 +149,7 @@ function parseDate(string) {
     match[5],
     match[6],
     0
-  );
+  ));
 }
 
 // Buffer reading helpers to help switching between endianness
